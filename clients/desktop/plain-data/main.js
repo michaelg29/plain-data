bdb790da13d8d0d21a7b596cf6e1df712aaa6b80
michaelg29 <michaelgriecocoding@gmail.com>
2019-07-14 00:32:06 -0400

    Created desktop application

---
+++ clients/desktop/plain-data/main.js
@@ -0,0 +1,25 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+// wait for app to be ready
+app.on('ready', function() {
+    // create window
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // load html into window
+    mainWindow.loadURL("index.html");
+
+    // quit app when closed
+    mainWindow.on('closed', function() {
+        app.quit();
+    });
+});
