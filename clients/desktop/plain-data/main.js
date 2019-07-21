251942a80af88bcd8683f49cb014aa0ece4e9e1a
michaelg29 <michaelgriecocoding@gmail.com>
2019-07-21 11:26:01 -0400

    Added jquery and basic files

--- clients/desktop/plain-data/main.js
+++ clients/desktop/plain-data/main.js
@@ -1,14 +1,17 @@
 const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
 const client = require('./js_utils/data_client')
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
+var online = false;
+
 // wait for app to be ready
 app.on('ready', function() {
-    //client.start();
-
     // create window
     mainWindow = new BrowserWindow({
         width: 800,
@@ -19,14 +22,49 @@ app.on('ready', function() {
     });
 
     // load html into window
-    mainWindow.loadURL("index.html");
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'content/index.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
 
     // quit app when closed
     mainWindow.on('closed', function() {
         app.quit();
     });
+
+    sendMsg('page:go','dashboard');
 });
 
-app.on('quit', function() {
-    client.cleanup();
+// catch window ready
+ipcMain.on('window:ready', function(e) {
+    //client.start();
 });
+
+// before exiting
+app.on('before-quit', function() {
+    //client.cleanup();
+})
+
+// on quit
+app.on('quit', function() {
+    
+});
+
+sendAlert = function(code) {
+    if (mainWindow) {
+        mainWindow.webContents.send(code);
+    }
+};
+
+sendMsg = function(code, data) {
+    if (mainWindow) {
+        console.log("sent");
+        mainWindow.webContents.send(code, data);
+    }
+};
+
+module.exports = {
+    sendAlert: this.sendAlert,
+    sendMsg: this.sendMsg,
+};
