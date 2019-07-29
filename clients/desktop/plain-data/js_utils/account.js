a72b6bc3ca4e9e10a6743667a5684fd9ed78feff
michaelg29 <michaelgriecocoding@gmail.com>
2019-07-28 23:58:39 -0400

    Scaffolded login function

--- clients/desktop/plain-data/js_utils/account.js
+++ clients/desktop/plain-data/js_utils/account.js
@@ -1,5 +1,16 @@
-function login(username, password) {
+const client = require('./global/data_client');
 
+function login(username, password) {
+    let req = {
+        "type": "Account",
+        "action": "login",
+        "values": {
+            "username": username,
+            "password": password
+        }
+    }
+
+    client.sendMsg(JSON.stringify(req));
 }
 
 module.exports = {
